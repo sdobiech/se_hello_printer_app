1c71e8236d52563955e6355818bcb0655fda6e43
sdobiech <tester@tester-m>
2021-04-10 08:08:29 +0000

    kontrola

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,8 +3,8 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Sylwia"
-msg = "Czesc, dasz rade!"
+moje_imie = "Natalia"
+msg = "Hello World!"
 
 @app.route('/')
 def index():
